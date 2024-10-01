59f4650e4e05526b9bbc6c7eb243094fef7b92a7
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-01 18:39:27 -0300

    add

--- routes/products.js
+++ routes/products.js
@@ -263,6 +263,22 @@ router.get('/category/:category/priceRange', productController.getPriceRangesByC
 
 
 
-// ... (outras rotas)
 
+
+router.get("/all-products", async (req, res) => {
+  try {
+    const products = await Product.find(); // Busca todos os produtos
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      products,
+    });
+  } catch (error) {
+    console.error("Erro ao obter produtos:", error);
+    res.status(500).json({
+      success: false,
+      message: "Erro ao obter produtos",
+    });
+  }
+});
 module.exports = router;
